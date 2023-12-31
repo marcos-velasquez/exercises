8152940ef02eb9c51d3e45a3bc9a68792d4f1d9c
Marcos Velasquez <marcos.svz28@gmail.com>
2023-12-31 13:20:25 -0400

    change quizService getQuiz promise to async/await

--- Quiz app/services/quiz.service.js
+++ Quiz app/services/quiz.service.js
@@ -2,20 +2,18 @@ import { Question } from "../models/question.model.js";
 import { Quiz } from "../models/quiz.model.js";
 
 export class QuizService {
-  getQuiz() {
-    return new Promise((resolve) => {
-      fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then((res) => res.json())
-        .then((data) => {
-          const questions = data.results.map((question) => {
-            return new Question(
-              question.question,
-              question.incorrect_answers,
-              question.correct_answer
-            );
-          });
-          resolve(new Quiz(questions));
-        });
+  #URL = "https://opentdb.com/api.php?amount=5&type=multiple";
+
+  async getQuiz() {
+    const res = await fetch(this.#URL);
+    const data = await res.json();
+    const questions = data.results.map((question) => {
+      return new Question(
+        question.question,
+        question.incorrect_answers,
+        question.correct_answer
+      );
     });
+    return new Quiz(questions);
   }
 }
